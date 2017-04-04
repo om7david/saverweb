1b4a96222b81060604e18d4277dc601c068e7fb1
David Hinojosa <om7david@gmail.com>
2017-04-04 08:00:51 -0500

    changed files to html

--- acerca.php
+++ acerca.php
@@ -1,81 +1,103 @@
-<?php include 'inc/header.html'; ?>
-<nav class="navbar navbar-inverse">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-4">
-                <div class="logo">
-                    <img src="img/saver-logo.svg" class="img-responsive">
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/saver.css">
+    <link href="https://fonts.googleapis.com/css?family=Fira+Sans:300,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <title>Saver</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-inverse">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="logo">
+                        <img src="img/saver-logo.svg" class="img-responsive">
+                    </div>
+                </div>
+                <div class="col-sm-8">
+                    <form class="navbar-form navbar-right">
+                        <button type="submit" class="btn btn-secondary">Registrarte</button>
+                    </form>
                 </div>
             </div>
-            <div class="col-sm-8">
-                <form class="navbar-form navbar-right">
-                    <button type="submit" class="btn btn-secondary">Registrarte</button>
-                </form>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="page-title text-center">
-                    <h1>Acerca de nosotros</h1>
-                    <h2>Conoce la herramienta que te va a llevar a la cima</h2>
-                </div>
-            </div>
-        </div>
-    </div>
-</nav>
-<section>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-6">
-                <img src="img/acercadesaver.jpg" class="img-responsive">
-            </div>
-            <div class="col-sm-6 text-large">
-                <p>Sed haec quis possit intrepidus aestimare tellus. Cum ceteris in veneratione tui montes, nascetur mus. Integer legentibus erat a ante historiarum dapibus. Cras mattis iudicium purus sit amet fermentum. Qui ipsorum lingua Celtae, nostra Galli appellantur. Quis aute iure reprehenderit in voluptate velit esse.</p>
-                <p>Tityre, tu patulae recubans sub tegmine fagi dolor. Quae vero auctorem tractata ab fiducia dicuntur. Prima luce, cum quibus mons aliud consensu ab eo.</p>
-                <a href="#" class="btn btn-primary">Registrarme</a>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="section-title">
-                    <h2>¿Qué esperas para registrarte?</h2>
-                    <h3 class="sub">Miles de personas ya estan usando Saver para encontrar más trabajo</h3>
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="page-title text-center">
+                        <h1>Acerca de nosotros</h1>
+                        <h2>Conoce la herramienta que te va a llevar a la cima</h2>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-sm-4">
-                <div class="service-box">
-                    <i class="material-icons md-48">face</i>
-                    <h3>Publica nuevas oportunidades</h3>
-                    <p>Salutantibus vitae elit libero, a pharetra augue. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Non equidem invideo, miror magis posuere velit aliquet.</p>
+    </nav>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <img src="img/acercadesaver.jpg" class="img-responsive">
+                </div>
+                <div class="col-sm-6 text-large">
+                    <p>Sed haec quis possit intrepidus aestimare tellus. Cum ceteris in veneratione tui montes, nascetur mus. Integer legentibus erat a ante historiarum dapibus. Cras mattis iudicium purus sit amet fermentum. Qui ipsorum lingua Celtae, nostra Galli appellantur. Quis aute iure reprehenderit in voluptate velit esse.</p>
+                    <p>Tityre, tu patulae recubans sub tegmine fagi dolor. Quae vero auctorem tractata ab fiducia dicuntur. Prima luce, cum quibus mons aliud consensu ab eo.</p>
+                    <a href="#" class="btn btn-primary">Registrarme</a>
                 </div>
             </div>
-            <div class="col-sm-4">
-                <div class="service-box">
-                    <i class="material-icons md-48">work</i>
-                    <h3>Encuentra más trabajo</h3>
-                    <p>Salutantibus vitae elit libero, a pharetra augue. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Non equidem invideo, miror magis posuere velit aliquet.</p>
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="section-title">
+                        <h2>¿Qué esperas para registrarte?</h2>
+                        <h3 class="sub">Miles de personas ya estan usando Saver para encontrar más trabajo</h3>
+                    </div>
                 </div>
             </div>
-            <div class="col-sm-4">
-                <div class="service-box">
-                    <i class="material-icons md-48">recent_actors</i>
-                    <h3>Llega a más gente</h3>
-                    <p>Salutantibus vitae elit libero, a pharetra augue. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Non equidem invideo, miror magis posuere velit aliquet.</p>
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="service-box">
+                        <i class="material-icons md-48">face</i>
+                        <h3>Publica nuevas oportunidades</h3>
+                        <p>Salutantibus vitae elit libero, a pharetra augue. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Non equidem invideo, miror magis posuere velit aliquet.</p>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="service-box">
+                        <i class="material-icons md-48">work</i>
+                        <h3>Encuentra más trabajo</h3>
+                        <p>Salutantibus vitae elit libero, a pharetra augue. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Non equidem invideo, miror magis posuere velit aliquet.</p>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="service-box">
+                        <i class="material-icons md-48">recent_actors</i>
+                        <h3>Llega a más gente</h3>
+                        <p>Salutantibus vitae elit libero, a pharetra augue. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Non equidem invideo, miror magis posuere velit aliquet.</p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
-<footer class="footer">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-12">
-                <a href="#">Terminos y Condiciones</a> |
-                <a href="#">Acerca de Saver</a>
+    </section>
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <a href="#">Terminos y Condiciones</a> |
+                    <a href="#">Acerca de Saver</a>
+                </div>
             </div>
         </div>
-    </div>
-</footer>
-<?php include 'inc/footer.html'; ?>
+    </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+
+</html>
